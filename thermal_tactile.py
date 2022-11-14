7989356b254cf38f1ada9af61dedb8817af2fa85
takuyajodai <j.takuya0625@icloud.com>
2022-11-14 21:37:37 +0900

    SOAをscvファイルで入力，出力まで実装

--- thermal_tactile.py
+++ thermal_tactile.py
@@ -30,15 +30,6 @@ import plot
 AI_ERR_HAPPENED     = 2
 
 
-def cal_temp(volt):
-        #B parameter equation
-        current = 0.000487
-        b = 3889
-        registance = volt / current
-        current_temp = (b / (math.log(registance) + 3.8334)) - 273.15
-
-        return current_temp
-
 #================================================================
 # Main function
 #================================================================
@@ -56,12 +47,54 @@ def main():
     AiSamplingCount = ctypes.c_long()               # The current number of samplings
     AiChannels = ctypes.c_short()                   # Number of the used channels
     AiTotalSamplingTimes = ctypes.c_long(0)         # The total number of samplings to be retrieved
+    AoChannel = 0                                   # 使用する出力チャンネル
 
     exp_flag = False                                # セッション中か
     state = 1                                       # 
-    temp = []
+    temp = []                                       # temp[0] : device temp, temp[1] : skin temp change, temp[2] : skin_temp
     run_once = 0
+    run_once_solenoid = 0
+    trial_count = 0                                 # 一試行ごとにインクリメント
+    state_text = ""
 
+    # parameter
+    ki = -0.0003
+    kp = -0.5
+
+    # temp cal
+    current = 0.000487
+    b = 3889
+
+    # PI control
+    temp_err_sum = 0
+
+    #----------------------------------------
+    # Declare function
+    #----------------------------------------
+
+    # 関数呼び出しのオーバヘッドをできるだけ削減するためにベタ打ち
+    """
+    def cal_temp(volt):
+        #B parameter equation
+        current = 0.000487
+        b = 3889
+        registance = volt / current
+        current_temp = (b / (math.log(registance) + 3.8334)) - 273.15
+
+        return current_temp
+
+    """
+    def pi_contorl(aimed_temp, current_temp, temp_err_sum):
+        # PIコントロール
+        temp_err = float(aimed_temp) - current_temp
+        temp_err_sum += temp_err
+        
+        Vo = (kp * temp_err) + (ki * temp_err_sum) + 2.5
+        ret.value = caio.AioSingleAoEx(aio_id, AoChannel, Vo)
+        if ret.value != 0:
+            caio.AioGetErrorString(ret.value, err_str)
+            print(f"AioSingleAoEx = {ret.value}:{err_str.value.decode('sjis')}")
+        return Vo
     #----------------------------------------
     # Initialization
     #----------------------------------------
@@ -119,6 +152,18 @@ def main():
         print(f"AioSetAiRangeAll = {ret.value} : {err_str.value.decode('sjis')}")
         caio.AioExit(aio_id)
         sys.exit()
+
+    #----------------------------------------
+    # Set the analog output range (0~5v固定)
+    #----------------------------------------
+    aoRange = int(0)
+    ret.value = caio.AioSetAoRangeAll(aio_id, aoRange)
+    if ret.value != 0:
+        caio.AioGetErrorString(ret, err_str)
+        print(f"AioSetAoRangeAll = {ret.value} : {err_str.value.decode('sjis')}")
+        caio.AioExit(aio_id)
+        sys.exit()
+
 
     #----------------------------------------
     # Step5 Set the transfer type
@@ -200,6 +245,16 @@ def main():
         sys.exit()
 
     #----------------------------------------
+    # SOAファイルよみこみ (csv)
+    #----------------------------------------
+    with open('./soa_input.csv') as f:
+        reader = csv.reader(f)
+        soa_list = [row for row in reader]
+        print(soa_list)
+        
+
+
+    #----------------------------------------
     # Start Converting
     #----------------------------------------
     ret.value = caio.AioStartAi(aio_id)
@@ -215,7 +270,7 @@ def main():
     #----------------------------------------
     # Get status of converting
     #----------------------------------------
-    while True:
+    while trial_count < len(soa_list):
         #----------------------------------------
         # Get the number of sampling data stored in the memory
         #----------------------------------------
@@ -257,42 +312,154 @@ def main():
 
         if exp_flag == True:
             #データ収集　(共通)
+            # temp[0] : device temp, temp[1] : skin temp change, temp[2] : skin_temp
             for i in range(AiChannels.value):
-                temp[i] = cal_temp(AiData[i])
+                volt = AiData[i]
+                registance = volt / current
+                current_temp = (b / (math.log(registance) + 3.8334)) - 273.15
+                #temp[i] = cal_temp(AiData[i])
+                temp[i] = current_temp
 
             if run_once == 0:
                 start = AiTotalSamplingTimes.value
+                start_time = time.perf_counter()
                 run_once = 1
+
+            # キャリブレーション
             if state == 1:
+                state_text = "WAITフェーズ"
                 end = AiTotalSamplingTimes.value
-                if (end - start >= 5000):
-                    print("waitいけたで")
+                end_time = time.perf_counter()
+                if (end - start >= 4000): 
+                    print(end_time - start_time)
                     state = 2
                     run_once = 0
+                    temp_err_sum = 0
+                Vo = pi_contorl(temp[2], temp[0], temp_err_sum)
+            
+            # 提示　先行のもの    
             elif state == 2:
                 end = AiTotalSamplingTimes.value
-                if (end - start >= 3000):
-                    print("doいけたで")
+                end_time = time.perf_counter()
+
+                soa = int(soa_list[trial_count][1])
+                # SOAが正の場合 触覚先行
+                if soa >= 0:
+                    ret.value = caio.AioOutputDoBit ( aio_id , 0 , 1 )
+                    time.sleep(0.01)
+                    ret.value = caio.AioOutputDoBit ( aio_id , 0 , 0 )
+                    
+                    state_text = "BEFORE DOフェーズ 触覚"
+                    print(end_time - start_time)
                     state = 3
                     run_once = 0
+                    temp_err_sum = 0
+                # SOAが負の場合 熱先行
+                else:
+                    state_text = "BEFORE DOフェーズ 熱"
+                    Vo = pi_contorl(temp[2] - 7, temp[0], temp_err_sum)
+                    if (end - start >= abs(soa)):
+                        print(end_time - start_time)
+                        state = 3
+                        run_once = 0
+                        temp_err_sum = 0
+
             elif state == 3:
                 end = AiTotalSamplingTimes.value
+                end_time = time.perf_counter()
+
+                soa = int(soa_list[trial_count][1])
+
+                # SOAが正の場合 触覚先行
+                if soa >= 0:
+                    state_text = "LATER DOフェーズ 熱"
+                    if (end - start >= abs(soa)):
+                        Vo = pi_contorl(temp[2] - 7, temp[0], temp_err_sum)
+                        if (end - start >= 3000 - abs(soa)):
+                            print(end_time - start_time)
+                            state = 4
+                            run_once = 0
+                            temp_err_sum = 0           
+                # SOAが負の場合 熱先行
+                else:
+                    state_text = "LATER DOフェーズ 触覚"
+                    if run_once_solenoid == 0:
+                        ret.value = caio.AioOutputDoBit ( aio_id , 0 , 1 )
+                        time.sleep(0.01)
+                        ret.value = caio.AioOutputDoBit ( aio_id , 0 , 0 )
+                        run_once_solenoid = 1
+                    Vo = pi_contorl(temp[2] - 7, temp[0], temp_err_sum)
+                    if (end - start >= 3000 - abs(soa)):
+                        print(end_time - start_time)
+                        state = 4
+                        run_once = 0
+                        run_once_solenoid = 0
+                        temp_err_sum = 0
+                    
+                    
+
+            elif state == 4:
+                state_text = "ANSフェーズ"
+                end = AiTotalSamplingTimes.value
+                end_time = time.perf_counter()
                 if (end - start >= 3000):
-                    print("ansいけたで")
+                    print(end_time - start_time)
                     state = 1
                     run_once = 0
+                    temp_err_sum = 0
+                    trial_count += 1
 
-            print("\r{:.3f}".format(temp[0]) + "℃  " + "{:.3f}".format(temp[1]) + "℃  " + "{:.3f}".format(temp[2]) + "℃  " + "{:.0f}".format(AiSamplingCount.value) + "回  " + "{:.0f}".format(AiTotalSamplingTimes.value) + "回  ", end = '', flush = True)
-    #----------------------------------------
+                Vo = pi_contorl(temp[2], temp[0], temp_err_sum)
+                
+
+            # セーフティ
+            if temp[0] <= 15 or temp[0] >= 45:
+                exp_flag = False
+
+            print("\rデバイス温 {:.3f}".format(temp[0]) + "℃  " +\
+                "皮膚温変化 {:.3f}".format(temp[1]) + "℃  " +\
+                "皮膚温 {:.3f}".format(temp[2]) + "℃  " +\
+                #"{:.0f}".format(AiSamplingCount.value) + "回  " +\
+                "サンプリング回数{:.0f}".format(AiTotalSamplingTimes.value) + "回  " +\
+                "{:.3f}".format(Vo) + "V  " +\
+                "トライアル{:.0f}".format(trial_count) + "回  " +\
+                state_text, end = ' ', flush = True)
+
+        else:
+            Vo = 2.5
+            ret.value = caio.AioSingleAoEx(aio_id, AoChannel, Vo)
+            if ret.value != 0:
+                caio.AioGetErrorString(ret.value, err_str)
+                print(f"AioSingleAoEx = {ret.value}:{err_str.value.decode('sjis')}")
+
+
+    #----------------------------------------     
     # Stop Converting
     #----------------------------------------
     print("\n\nStop Converting\n")
+    Vo = 2.5
+    ret.value = caio.AioSingleAoEx(aio_id, AoChannel, Vo)
+    if ret.value != 0:
+        caio.AioGetErrorString(ret.value, err_str)
+        print(f"AioSingleAoEx = {ret.value}:{err_str.value.decode('sjis')}")
     ret.value = caio.AioStopAi(aio_id)
     if ret.value != 0:
         caio.AioGetErrorString(ret, err_str)
         print(f"AioStopAi = {ret.value} : {err_str.value.decode('sjis')}")
         caio.AioExit(aio_id)
         sys.exit()
+
+    #ファイル出力
+    """
+    f = open('./result.csv', 'w') 
+    header = ["time(sec)", "temp(℃)"]
+    writer = csv.writer(f)
+    writer.writerow(header)
+    result = zip(time_data, temp_data)
+    writer.writerows(result)
+    f.close()
+    """
+
     #----------------------------------------
     # Exit the device
     #----------------------------------------
